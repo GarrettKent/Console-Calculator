d0ccbdaf50f3c5c0d7961cf6c22c7c45bac07b68
GarrettKent <46579862+GarrettKent@users.noreply.github.com>
2019-08-05 01:11:51 -0500

    Add files via upload

---
+++ Giraffe/Giraffe/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A Number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter An Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter Another Number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Please enter an operator");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
